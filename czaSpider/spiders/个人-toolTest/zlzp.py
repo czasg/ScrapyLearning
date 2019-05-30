810359c6d7b4d0df31dbad85b0e88ec88e636b26
cza <chenziangsg@163.com>
2019-05-30 21:26:24 +0800

    upload

--- czaSpider/spiders/个人-toolTest/zlzp.py
+++ czaSpider/spiders/个人-toolTest/zlzp.py
@@ -5,6 +5,3 @@ from czaSpider.czaTools import *
 class MySpider(czaSpider):
     name = "zlzp-jobPosition"
     author = "czaOrz"
-    collName = get_collection_name(name, timeStr=True)
-    dbName = get_database_name(name)
-    # custom_settings = get_custom_settings(name)
--- czaSpider/spiders/个人-toolTest/测试专用.py
+++ czaSpider/spiders/个人-toolTest/测试专用.py
@@ -2,6 +2,14 @@ from czaSpider.czaBaseSpider import IOCO
 from czaSpider.czaTools import *
 
 
+class testMiddleWare():
+    def process_response(self, request, response, spider):
+        print(response.text)
+        return response
+    def process_request(self, request, spider):
+        request.meta["hello"] = "world"
+        pass
+
 class MySpider(IOCO):
     name = "world-test"
 
@@ -12,12 +20,15 @@ class MySpider(IOCO):
         "pageNo": str(page),
         "pageSize": "10",
     }
+    custom_settings = {"DOWNLOADER_MIDDLEWARES":
+                           {"czaSpider.spiders.个人-toolTest.测试专用.testMiddleWare":100}}
 
     def start_requests(self):
         yield FormRequest(self.url, formdata=self.data)
 
     def parse(self, response):
-        print(response.text)
+        print(response.meta["hello"])
+        # print(response.text)
 
 
 if __name__ == "__main__":
