961af99e967ce8177353c2a246ab2a25722fcf5f
cza <chenziangsg@163.com>
2019-06-13 23:18:55 +0800

    upload

--- czaSpider/dump/bootstrap_test/blogs/apis.py
+++ czaSpider/dump/bootstrap_test/blogs/apis.py
@@ -1,49 +1,74 @@
 import json
-import logging
 
 from aiohttp import web
 
 from handler import get, post
 from tools import *
 
-
 logger = logging.getLogger(__name__)
+
+
+# 模板页面 #
 
 @get('/')
 async def index():
     return {'__template__': 'index.html'}
 
 
-@get('/test')
-async def login():
-    print('login')
-    return {'__template__': 'index.html'}
-
-@get('/signout')
-def signout(request):
-    referer = request.headers.get('Referer')
-    res = web.HTTPFound(referer or '/')
-    res.set_cookie(COOKIE_NAME, '-deleted-', max_age=0)
-    logger.info('user signed out!')
-    return res
+# 登陆认证、注册、退出模块 #
 
 @post('/api/authenticate')
 async def authenticate(*, email, passwd):
     if not email or not passwd:
-        raise APIResourceError('apis-authenticate', '请输入有效的: 用户名/密码')
+        raise APIResourceError('请输入有效的: 用户名/密码', 'apis-authenticate')
     user = await User.findAll('email=?', [email])
     if len(user) == 0:
-        raise APIResourceError('apis-authenticate', '用户名不存在')
+        raise APIResourceError('用户名不存在', 'apis-authenticate')
     user = user[0]
     sha1 = hashlib.sha1()
     sha1.update(user.id.encode())
     sha1.update(b':')
     sha1.update(passwd.encode())
     if user.passwd != sha1.hexdigest():
-        raise APIResourceError('apis-authenticate', '密码错误')
-    res = web.Response()
-    res.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400)
+        raise APIResourceError('密码错误', 'apis-authenticate')
+    webResponse = web.Response()
+    webResponse.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400)
     user.passwd = '******'
-    res.content_type = 'application/json'
-    res.body = json.dumps(user, ensure_ascii=False).encode()
-    return res
+    webResponse.content_type = 'application/json'
+    webResponse.body = json.dumps(user, ensure_ascii=False).encode()
+    return webResponse
+
+
+@get('/signout')
+def signout(request):
+    referer = request.headers.get('Referer')
+    webResponse = web.HTTPFound(referer or '/')
+    webResponse.set_cookie(COOKIE_NAME, '-deleted-', max_age=0)
+    logger.info('user signed out!')
+    return webResponse
+
+
+# json请求接口 #
+
+@get('/api/get/users')
+async def api_get_users(*, page='1'):
+    page_index = get_page_index(page)
+    num = await User.findNumber('count(id)')
+    p = Pager(num, page_index)
+    if num == 0:
+        return dict(page=0, users=())
+    users = await User.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    for u in users:
+        u.passwd = '******'
+    return dict(page=p, users=users)
+
+
+@get('/api/get/blogs')
+async def api_get_blogs(*, page='1'):
+    page_index = get_page_index(page)
+    num = await Blog.findNumber('count(id)')
+    p = Pager(num, page_index)
+    if num == 0:
+        return dict(page=0, users=())
+    blogs = await Blog.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    return dict(page=p, blogs=blogs)
