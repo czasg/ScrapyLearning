9783b00e07e858c6abd2137507136ee96119871b
cza <chenziangsg@163.com>
2019-06-29 13:26:43 +0800

    add test project

--- czaSpider/spiders/hardSpider/中国裁判文书网/test2.py
+++ czaSpider/spiders/hardSpider/中国裁判文书网/test2.py
@@ -9,7 +9,8 @@ detail_url = "http://wenshu.court.gov.cn/CreateContentJS/CreateContentJS.aspx"
 
 # entrance = "http://wenshu.court.gov.cn/list/list/?sorttype=1&number=3YHLPLNF" \
 #            "&guid=5ef7e9dc-73c0-b781cc87-3397a46b1b03&conditions=searchWord+QWJS+++%E5%85%A8%E6%96%87%E6%A3%80%E7%B4%A2:%E5%A4%84%E7%BD%9A"
-entrance = "http://wenshu.court.gov.cn/list/list/?sorttype=1"
+# entrance = "http://wenshu.court.gov.cn/list/list/?sorttype=1"
+entrance = "http://wenshu.court.gov.cn/List/List?sorttype=1&conditions=searchWord+1++%E5%88%91%E4%BA%8B%E6%A1%88%E4%BB%B6+%E6%A1%88%E4%BB%B6%E7%B1%BB%E5%9E%8B:%E5%88%91%E4%BA%8B%E6%A1%88%E4%BB%B6"
 anti_headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
 }
@@ -54,8 +55,11 @@ class AntiJS:
     def anti_first(self):
         print("enter AntiJS...")
         javascript_code = self.response.text
+        # dynamicUrl, wzWsQuestion, wzWsFactor = \
+        #     re.search('dynamicurl="(.*?)".*?wzwsquestion="(.*?)".*?wzwsfactor="(.*?)"',
+        #               javascript_code).groups()
         dynamicUrl, wzWsQuestion, wzWsFactor = \
-            re.search('dynamicurl="(.*?)".*?wzwsquestion="(.*?)".*?wzwsfactor="(.*?)"',
+            re.search('dynamicurl\|(.*?)\|.*?wzwsquestion\|(.*?)\|.*?wzwsfactor\|(.*?)\|',
                       javascript_code).groups()
         wzWsChallenge = anti_first.call('anti_first', wzWsQuestion, wzWsFactor)
         dynamicUrl = self.response.urljoin(dynamicUrl) + "?wzwschallenge=" + wzWsChallenge
@@ -87,11 +91,13 @@ class MySpider(IOCO):
         print('enter No antiJS...')
         cookie = response.headers['Set-Cookie'].decode()
         vjkl5 = re.search('vjkl5=(.*?);', cookie).group(1)
-        vl5x, number, guid = anti_second.call('anti_second', vjkl5)
+        # vl5x, number, guid = anti_second.call('anti_second', vjkl5)  # todo,
+        vl5x, guid, number = anti_second.call('anti_second', vjkl5)
         formData = refresh_formData(PAGE, vl5x, number, guid)
         print(formData)
         yield FormRequest(list_url, self.parse2, headers=anti_headers, formdata=formData, dont_filter=True)
 
+    @AntiJS.auto
     def parse2(self, response):
         print(response.text)
         # return
