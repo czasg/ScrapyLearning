6ae2f96ad117e7eb898c796f9288c6f960b141aa
cza <chenziangsg@163.com>
2019-07-02 00:06:37 +0800

    add test project

--- czaSpider/todo.py
+++ czaSpider/todo.py
@@ -23,10 +23,10 @@ __FILE__ = "Unfinished Todo"
 2019.06.06 - <input type="text" ref="input1"/>  this.$refs.input1.value ="22"; 减少获取dom节点的消耗  - Vue props, mounted()
 2019.06.26 - 插入视屏是个啥玩意：<video src="http://vjs.zencdn.net/v/oceans.mp4" type="video/mp4" autoplay="autoplay" controls="controls" loop="-1" poster="false.png">
 2019.07.01 - 现阶段的主要任务就是编写文档，储存补给站
-2019.07.01 - 发展历史线，可以使用ol/li有序表？还是ul无序表
 """
 
 """DONE!
+2019.07.01 - 发展历史线，可以使用ol/li有序表？还是ul无序表
 2019.06.30 - 邮件模块记得拿到手，别浪费了
 2019.06.18 - 微信可以了解验证码，微博可以练selenium -- 没有遇到验证码，说明有cookie，下午可以试一试
 2019.06.26 - 监听回车键进行登陆完成
