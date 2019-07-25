6a3dc7051e46682c37cb0dac748dd680831287e9
cza <czasg0.0>
2019-07-25 17:46:36 +0800

    add code

---
+++ czaSpider/spiders/hardSpider/中国裁判文书网/20190725改版/test.py
@@ -0,0 +1,83 @@
+import execjs
+
+from czaSpider.czaBaseSpider import IOCO
+from czaSpider.czaTools import *
+
+try:
+    with open('zgcpwsw2.js', encoding='utf-8') as f:
+        js = f.read()
+        anti_second = execjs.compile(js)
+
+    with open('zgcpwsw3.js', encoding='utf-8') as f:
+        js = f.read()
+        anti_third = execjs.compile(js)
+except:
+    pass
+
+PAGE = 1
+
+
+def refresh_formData(page, vl5x, number, guid):
+    formData = {
+        "Param": "全文检索:合同",
+        "Index": str(page),
+        "Page": "10",
+        "Order": "法院层级",
+        "Direction": "asc",
+        "vl5x": str(vl5x),
+        "number": str(number),
+        "guid": str(guid),
+    }
+    return formData
+
+
+entrance = "http://wenshu.court.gov.cn/List/List?sorttype=1&conditions=searchWord+1++%E8%A1%8C%E6%94%BF%E6%A1%88%E4%BB%B6+%E6%A1%88%E4%BB%B6%E7%B1%BB%E5%9E%8B:%E8%A1%8C%E6%94%BF%E6%A1%88%E4%BB%B6"
+anti_headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
+}
+detail_url = "http://wenshu.court.gov.cn/CreateContentJS/CreateContentJS.aspx"
+list_url = "http://wenshu.court.gov.cn/List/ListContent"
+
+
+class MySpider(IOCO):
+    name = "chinaReferee20190725-government"
+
+    custom_settings = {'HTTPERROR_ALLOWED_CODES': [503, 502]}
+
+    def start_requests(self):
+        yield Request(entrance, headers=anti_headers, dont_filter=True)
+
+    def parse(self, response):
+        print(response.request.cookies)
+        print(response.headers)
+        vjkl5 = re.search("vjkl5=(.*?);", str(response.headers)).group(1)
+        print('获取到了vjkl5:', vjkl5)
+        vl5x, guid, number = anti_second.call('anti_second', vjkl5)
+        number = 'wens'
+        formData = refresh_formData(PAGE, vl5x, number, guid)
+        print(formData)
+        global anti_headers
+        anti_headers['Cookie'] = response.headers["Set-Cookie"].decode()
+        yield FormRequest(list_url, self.parse2, headers=anti_headers, formdata=formData, dont_filter=True)
+
+    def parse2(self, response):
+        print(response.text)
+        content = json.loads(json.loads(response.text))
+        RunEval = content[0]['RunEval']
+        detail_urls = []
+        for i in range(1, len(content)):
+            print(content[i])
+            try:
+                docId = content[i]['文书ID']
+            except KeyError:
+                print('No 文书ID, request again!')
+                yield Request(entrance, self.parse, headers=anti_headers, dont_filter=True)
+                return
+            DocID = anti_third.call('anti_third', RunEval, docId)
+            save_url = detail_url + "?DocID=" + DocID
+            detail_urls.append(save_url)
+        print(detail_urls)
+
+
+if __name__ == '__main__':
+    MySpider.cza_run_spider()
