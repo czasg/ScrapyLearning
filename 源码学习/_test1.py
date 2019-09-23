bc28be41f896e94f9c3f600f13104fe5cd5e17a4
cza <czasg0.0>
2019-09-23 09:09:54 +0800

    转移源码解读模块

--- 源码学习/_test1.py
+++
@@ -1,9 +0,0 @@
-import signal
-from pydispatch import dispatcher
-from _test import *
-
-if __name__ == '__main__':
-    started = object()
-    stopped = object()
-    signal_test(started, stopped)
-    dispatcher.send(signal=stopped)  # 卧槽，这个模块也太好用了把
