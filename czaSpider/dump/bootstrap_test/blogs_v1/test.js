130e103f24a7ec8af407976f4ea9461ebe839970
cza <czasg0.0>
2019-09-29 13:22:53 +0800

    聊天

--- czaSpider/dump/bootstrap_test/blogs_v1/test.js
+++ czaSpider/dump/bootstrap_test/blogs_v1/test.js
@@ -1,44 +1,79 @@
+/*
+维护users字典，存储用户的图片信息
+*/
 class User {
-    constructor({name, age}){
-        this.name = name;
-        this.age = age;
+    constructor({user_image, user_name, is_master, snow_key, unreadInfo=0}){
+        this.user_image = user_image;
+        this.user_name = user_name;
+        this.is_master = is_master;
+        this.snow_key = snow_key;
+        this.unreadInfo = unreadInfo;  // 定义未读信息
     }
 }
 
-
 $(function(){
-    new Vue({
+    test = new Vue({
         el: '#TEST',
         data(){
             return {
-                chat_msg: [
+                sayingWord: '',
+                users: {},  // snow_key : User
+                messages: {},
+                sessions: [],
+                current_chat: '',
+            }
+        },
+        mounted(){
+            var users_info = [
                     {
                         user_image: 'static/img/user.png',
                         user_name: 'cza',
                         user_say: 'hello world',
                         is_master: false,
+                        snow_key: '123',
+                        to: '123456',
                     },
                     {
                         user_image: 'static/img/user.png',
                         user_name: 'xiaoming',
                         user_say: 'welcome to WebChatwelcome to WebChatwelcome to WebChatwelcome to WebChatwelcome to WebChatwelcome to WebChatwelcome to WebChatwelcome to WebChatwelcome to WebChatwelcome to WebChatwelcome to WebChatwelcome to WebChatwelcome to WebChatwelcome to WebChatwelcome to WebChatwelcome to WebChatwelcome to WebChat',
                         is_master: true,
+                        snow_key: '123456',
+                        to: '123',
                     },
                     {
                         user_image: 'static/img/user.png',
                         user_name: 'lihong',
                         user_say: 'This is a Test Chat',
                         is_master: false,
+                        snow_key: '123123',
+                        to: '123456',
                     },
                     {
                         user_image: 'static/img/user.png',
                         user_name: 'lihong',
                         user_say: 'Are You OK!',
                         is_master: false,
+                        snow_key: '123123',
+                        to: '123456',
                     }
-                ],
-                sayingWord: ''
-            }
+                ];
+            users_info.forEach(user_info => {
+                if (!user_info.is_master) {
+                    if (this.sessions.indexOf(user_info.snow_key) === -1) {
+                        this.sessions.push(user_info.snow_key);
+                    }
+                }
+                if (this.users[user_info.snow_key] === undefined) {
+                    this.users[user_info.snow_key] = new User(user_info);  //这里存的数据太多了
+                    this.messages[user_info.snow_key] = [];
+                }
+                if (user_info.is_master) {
+                    this.messages[user_info.to].push(user_info);
+                } else {
+                    this.messages[user_info.snow_key].push(user_info);
+                }
+            })
         },
         methods: {
             saying: function(){
@@ -46,7 +81,7 @@ $(function(){
                     alert('输入不能为空');
                     return
                 }
-                this.chat_msg.push({
+                this.messages[this.current_chat].push({
                     user_image: 'static/img/user.png',
                     user_name: 'lihong',
                     user_say: this.sayingWord,
@@ -56,6 +91,9 @@ $(function(){
                 $('#chat-media-list').parent().animate({
                     scrollTop: $('#chat-media-list').height()
                 }, 1000);
+            },
+            choose_session: function(snow_key){
+                this.current_chat = snow_key;
             }
         }
     })
