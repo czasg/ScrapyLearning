5d45678ed6d0e70bf2c40ba9f03af3858283298f
cza <chenziangsg@163.com>
2019-10-28 22:25:00 +0800

    update

--- 临时/news/cock/views.py
+++ 临时/news/cock/views.py
@@ -100,12 +100,15 @@ def api_get_map_data_v1(request):
 
 @miniCache(60 * 60 * 3)
 def api_get_operation_data(request):
-    task_name = request.GET.get('task_name', None)
-    if not task_name or task_name not in multi_table: return Http404()
-    now = get_now_datetime()
-    init(task_name, '{}.01'.format(now.year), version=multi_table[task_name])
-    return JsonResponse({
-        'data_summary': get_data_first(),
-        'data_statistical': get_data_second(),
-        'data_bar': get_data_third(),
-    })
+    # task_name = request.GET.get('task_name', None)
+    # if not task_name or task_name not in multi_table: return Http404()
+    # now = get_now_datetime()
+    # init(task_name, '{}.01'.format(now.year), version=multi_table[task_name])
+    # return JsonResponse({
+    #     'data_summary': get_data_first(),
+    #     'data_statistical': get_data_second(),
+    #     'data_bar': get_data_third(),
+    # })
+    test = """{"data_summary": [["\u8bda\u4fe1\u6570\u636e\u603b\u8bb0\u5f55", 25229], ["\u8bda\u4fe1\u6570\u636e\u603b\u4efb\u52a1", 843], ["\u8bda\u4fe1\u6570\u636e\u6709\u8fc7\u8fd0\u7ef4\u8bb0\u5f55\u7684\u4efb\u52a1", 796], ["\u8bda\u4fe1\u6570\u636e\u5904\u4e8e\u5e9f\u5f03\u72b6\u6001\u7684\u4efb\u52a1", 333], ["\u8bda\u4fe1\u6570\u636e\u5386\u53f2\u5165\u5e93\u5b8c\u6210\u4efb\u52a1", 724], ["\u5165\u5e93\u4efb\u52a1\u4e2d\u8fd0\u7ef4\u53d1\u73b0\u5f02\u5e38\u7684\u4efb\u52a1", 493, "68%"], ["\u5165\u5e93\u4efb\u52a1\u4e2d\u8fd0\u7ef4\u5e9f\u5f03\u7684\u4efb\u52a1", 258, "35%"]], "data_statistical": [["mean", 2.7459584295612007, 9.930715935334874, "27%"], ["std", 1.9424923299646777, 3.7999517169386405, "-"], ["min", 1.0, 2.0, "-"], ["50%", 2.0, 9.0, "-"], ["max", 15.0, 19.0, "-"]], "data_bar": [["2019\u5e741\u6708", "2019\u5e742\u6708", "2019\u5e743\u6708", "2019\u5e744\u6708", "2019\u5e745\u6708", "2019\u5e746\u6708", "2019\u5e747\u6708", "2019\u5e748\u6708", "2019\u5e749\u6708", "2019\u5e7410\u6708"], [0, 0, 0, 0, 144, 37, 50, 15, 0, 12], [97, 2, 0, 38, 516, 208, 133, 111, 106, 38], [596, 596, 596, 596, 671, 531, 515, 475, 471, 479]]}"""
+    test = json.loads(test)
+    return JsonResponse(test)
