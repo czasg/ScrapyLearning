ca459b9346665d37435783f4272ce9c4d464b8cb
cza <czasg0.0>
2019-11-04 17:59:40 +0800

    update

---
+++ czaSpider/dump/selenuim_test/中文文档.py
@@ -0,0 +1,12 @@
+__file__ = 'Word'
+
+"""------------------------------------------------------------------------------------------------------------------------------------
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get("http://www.python.org")
+
+
+
+"""
