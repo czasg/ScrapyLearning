1b34f28ea10cb1d719d9d1bb9c4645ed60a45df9
cza <chenziangsg@163.com>
2019-11-07 00:04:50 +0800

    add todo

--- todo.py
+++ todo.py
@@ -30,6 +30,7 @@ __FILE__ = "Unfinished Schedule"
 2019.11.06 - 在tool中可以加入list或者字典的一些骚方法。自己写一个也是极好的
 2019.11.06 - logging - 模块也需要加一个
 2019.11.07 - 我去，scrapy包中真是有好多好东西呀。可以看着拿呀。比如对url的一些处理，不就又可以增加了嘛
+2019.11.07 - logging好文 - https://cloud.tencent.com/developer/section/1369390
 """
 
 """DONE!
